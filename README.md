00d21d600a33b6bea834dbab668455cafce82358
Mosh Feu <moshfeu@gmail.com>
2019-10-21 22:49:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## local-serve
+
+Run a nodejs server that exposed to everyone in the network.
+The use case is to transfer files from another device to this one.
+Also, it uses [`serve`](https://github.com/zeit/serve) to serve the files folder to the network
+
+## How to run?
+
+By default, the files will stored in a _files_ folder.
+
+```
+yarn
+yarn start
+```
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const handler = require('serve-handler');
+const fileUpload = require('express-fileupload');
+const { resolve } = require('path');
+const dns = require('dns');
+const os = require('os');
+const port = 5000;
+
+const app = express();
+app.use(fileUpload());
+
+const targetFolder = `${__dirname}/files`;
+
+app.post('/upload', async (req, res) => {
+  try {
+    console.log('got request');
+    const { file } = req.files;
+    try {
+      await file.mv(`${targetFolder}/${file.name}`);
+    } catch (error) {
+      console.error(error);
+    }
+    res.send('' + ((Object.keys(req.files) || []).length));
+  } catch (error) {
+    console.error(error);
+  }
+})
+
+app.use('/files', (request, response) => handler(request, response, {
+  public: targetFolder
+}));
+
+app.get('/', (request, response) => {
+  response.sendFile(resolve('index.html'));
+})
+
+app.listen(port, () => {
+  dns.lookup(os.hostname(), (_, add) => {
+    console.log(`Running at http://${add}:${port}`);
+  })
+});
